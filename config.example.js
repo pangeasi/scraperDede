632c8adbab06be6335475ca8d9b662e4df34af8a
Dani Ballesteros <pangeanoteniafronteras@gmail.com>
2019-06-08 23:00:21 +0200

    add config example

---
+++ config.example.js
@@ -0,0 +1,18 @@
+module.exports = {
+    cookie: [
+        {name: 'PHPSESSID',
+        value: '',
+        domain: '',
+        path: '/',
+        expires: 1605376742.173697,
+        size: 35,
+        httpOnly: true,
+        secure: false,
+        session: false }
+        ],
+        TELEGRAM_TOKEN: 'YOUR_KEY',
+        MASTODON_TOKEN: 'YOUR_KEY',
+        THEMOVIEDB_API_TOKEN: 'YOUR_KEY',
+        LOGIN_VERYSTREAM_API: 'YOUR_KEY',
+        KEY_VERYSTREAM_API: 'YOUR_KEY'
+}
